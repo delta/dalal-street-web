a4cf8adde3740fe8c2136b058c1b8434323b2ea2
coderick14 <bhattacharyya.rick14@gmail.com>
2018-01-12 14:50:01 +0530

    trying to set the ball rolling

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   cache: true,
-  entry: ["babel-polyfill", "./public/src/index.ts"],
+  entry: ["babel-polyfill", "./public/src/index.tsx"],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
@@ -13,17 +13,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         include: [
             path.resolve(__dirname, 'public/src'),
             path.resolve(__dirname, 'public/proto_build'),
         ],
         exclude: /node_modules/,
         loaders: ["babel-loader", "ts-loader"], // tsc converts to es6. convert that to es5
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'ts-protoc-gen')
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
       }
     ]
   },
   resolve: {
-    extensions: [".ts", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
 };
