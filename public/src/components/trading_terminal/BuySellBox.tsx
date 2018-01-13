12dc835dffb061745e4a9a70c8f70b83151a38ea
coderick14 <bhattacharyya.rick14@gmail.com>
2018-01-13 22:46:24 +0530

    Clean up some shit in ticker

--- public/src/components/trading_terminal/BuySellBox.tsx
+++ public/src/components/trading_terminal/BuySellBox.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 
-export interface BuySellBoxProps { company_id: any; current_price: any; }
+export interface BuySellBoxProps { companyId: any; currentPrice: any; }
 
 export class BuySellBox extends React.Component<BuySellBoxProps, {}> {
-    
 
     render() {
         return (<h1>hi</h1>);     
--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,26 +1,45 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
-import { BuySellBox } from "./components/BuySellBox";
-import { Navbar } from "./components/Navbar";
-import { TickerBar } from "./components/TickerBar"
+import { BuySellBox } from "./components/trading_terminal/BuySellBox";
+import { Navbar } from "./components/common/Navbar";
+import { TickerBar } from "./components/common/TickerBar"
 
 
-// ReactDOM.render(
-//     //<Hello compiler="TypeScript" framework="React" />,
-//     //<Ticker company_name="Google" current_price="1000" previous_price="980" />,
-//     //<BuySellBox company_id="Lol" current_price="123"/>,
-//     document.getElementById("testreact")
-// );
-
+let data = [
+	{
+		"companyName": "Amazon",
+		"currentPrice": 100,
+		"previousPrice": 10
+	},
+	{
+		"companyName": "Amazon1",
+		"currentPrice": 100,
+		"previousPrice": 10
+	},
+	{
+		"companyName": "Amazon2",
+		"currentPrice": 100,
+		"previousPrice": 10
+	},
+	{
+		"companyName": "Amazon3",
+		"currentPrice": 100,
+		"previousPrice": 10
+	},
+	{
+		"companyName": "Amazon4",
+		"currentPrice": 100,
+		"previousPrice": 10
+	}
+];
 
 ReactDOM.render(
-    <Navbar/>,
+    <Navbar />,
     document.getElementById("navbar")
 );
 
-// ReactDOM.render(
-//     <TickerBar data=/>,
-//     document.getElementById("navbar")
-// );
+ReactDOM.render(
+    <TickerBar stocks={data} />,
+    document.getElementById("ticker-bar")
+);
