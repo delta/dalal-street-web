f12c0b98cc44efb15c1051502a172bf4dc892b6c
paiakshay998 <paiakshay998@gmail.com>
2018-01-13 21:07:47 +0530

    added ticker and stuff

---
+++ public/src/components/BuySellBox.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface BuySellBoxProps { company_id: any; current_price: any; }
+
+export class BuySellBox extends React.Component<BuySellBoxProps, {}> {
+    
+
+    render() {
+        return (<h1>hi</h1>);     
+    }
+}
