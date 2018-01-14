856fe6c86c157a5f586e72f552d0e31771e9707f
paiakshay998 <paiakshay998@gmail.com>
2018-01-14 17:44:09 +0530

    colorful orders

--- public/src/components/trading_terminal/PlaceOrderBox.tsx
+++ public/src/components/trading_terminal/PlaceOrderBox.tsx
@@ -37,67 +37,67 @@ export class PlaceOrderBox extends React.Component<PlaceOrderBoxProps, {}> {
 					<a className="item" data-tab="limit">Limit</a>
 					<a className="item" data-tab="stoploss">Stoploss</a>
 				</div>
-				<div className="ui tab segment inverted active" data-tab="market">
+				<div className="ui tab inverted active" data-tab="market">
 					<div className="ui top attached tabular menu inverted">
 						<a className="active item green" data-tab="market/buy">BUY</a>
 						<a className="item red" data-tab="market/sell">SELL</a>
 					</div>
-					<div className="ui bottom attached active tab segment" data-tab="market/buy">
-						<div className="ui input focus">
+					<div className="ui bottom attached active tab segment inverted" data-tab="market/buy">
+						<div className="ui input  ">
 							<input id="market-buy-count" placeholder="Number of stocks" type="text" />
 						</div>
 						<button className="ui inverted green button" onClick={e => this.handleOrder(e,"market","buy")}>BUY</button>
 					</div>
-					<div className="ui bottom attached tab segment" data-tab="market/sell">
-						<div className="ui input focus">
+					<div className="ui bottom attached tab segment inverted" data-tab="market/sell">
+						<div className="ui input  ">
 							<input id="market-sell-count" placeholder="Number of stocks" type="text" />
 						</div>
 						<button className="ui inverted red button" onClick={e => this.handleOrder(e,"market","sell")}>SELL</button>
 					</div>
 				</div>
-				<div className="ui tab segment inverted" data-tab="limit">
+				<div className="ui tab inverted" data-tab="limit">
 					<div className="ui top attached tabular menu inverted">
 						<a className="item active green" data-tab="limit/buy">BUY</a>
 						<a className="item red" data-tab="limit/sell">SELL</a>
 					</div>
-					<div className="ui bottom attached tab segment active" data-tab="limit/buy">
-						<div className="ui input focus">
+					<div className="ui bottom attached tab segment active inverted" data-tab="limit/buy">
+						<div className="ui input  ">
 							<input id="limit-buy-count" placeholder="Number of stocks" type="text" />
 						</div>
-						<div className="ui input focus">
+						<div className="ui input  ">
 							<input id="limit-buy-price" placeholder="Limit Price" type="text" />
 						</div>
 						<button className="ui inverted green button" onClick={e => this.handleOrder(e,"limit","buy")}>BUY</button>
 					</div>
-					<div className="ui bottom attached tab segment" data-tab="limit/sell">
-						<div className="ui input focus">
+					<div className="ui bottom attached tab segment inverted" data-tab="limit/sell">
+						<div className="ui input  ">
 							<input id="limit-sell-count" placeholder="Number of stocks" type="text" />
 						</div>
-						<div className="ui input focus">
+						<div className="ui input  ">
 							<input id="limit-sell-price" placeholder="Limit Price" type="text" />
 						</div>
 						<button className="ui inverted red button" onClick={e => this.handleOrder(e,"limit","sell")}>SELL</button>
 					</div>
 				</div>
-				<div className="ui tab segment inverted" data-tab="stoploss">
+				<div className="ui tab inverted" data-tab="stoploss">
 					<div className="ui top attached tabular menu inverted">
 						<a className="item active green" data-tab="stoploss/buy">BUY</a>
 						<a className="item red" data-tab="stoploss/sell">SELL</a>
 					</div>
-					<div className="ui bottom attached tab segment active" data-tab="stoploss/buy">
-						<div className="ui input focus">
+					<div className="ui bottom attached tab segment active inverted" data-tab="stoploss/buy">
+						<div className="ui input  ">
 							<input id="stoploss-buy-count" placeholder="Number of stocks" type="text" />
 						</div>
-						<div className="ui input focus">
+						<div className="ui input  ">
 							<input id="stoploss-buy-price" placeholder="Stoploss Price" type="text" />
 						</div>
 						<button className="ui inverted green button" onClick={e => this.handleOrder(e,"stoploss","buy")}>BUY</button>
 					</div>
-					<div className="ui bottom attached tab segment" data-tab="stoploss/sell">
-						<div className="ui input focus">
+					<div className="ui bottom attached tab segment inverted" data-tab="stoploss/sell">
+						<div className="ui input  ">
 							<input id="stoploss-sell-count" placeholder="Number of stocks" type="text" />
 						</div>
-						<div className="ui input focus">
+						<div className="ui input  ">
 							<input id="stoploss-sell-price" placeholder="Stoploss Price" type="text" />
 						</div>
 						<button className="ui inverted red button" onClick={e => this.handleOrder(e,"stoploss","sell")}>SELL</button>
