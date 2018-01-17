f36d90b9629cd54baf8bede317812138d02b28d3
paiakshay998 <paiakshay998@gmail.com>
2018-01-17 22:48:13 +0530

    shiny buttons

--- public/src/components/common/Navbar.tsx
+++ public/src/components/common/Navbar.tsx
@@ -1,44 +1,51 @@
 import * as React from "react";
+import * as $ from 'jquery';
 import { Link } from "react-router-dom";
 
 export interface NavProps {}
 
 export class Navbar extends React.Component<NavProps, {}> {
-    render(){
+	
+	handleClick(event: any) {
+		$("#navbar a").removeClass("active");
+		event.target.className += " active";
+	}
+	
+	render(){
 
         return(
         <div id="navbar" className="ui sidebar inverted labeled icon left vertical menu uncover visible">
-		<Link to="/trade" className="item active">
+		<Link to="/trade" className="item active" onClick={e => this.handleClick(e)}>
 			<i className="rupee icon"></i>
 			Trade
 		</Link>
-		<Link to="/portfolio" className="item">
+		<Link to="/portfolio" className="item" onClick={e => this.handleClick(e)}>
 			<i className="book icon"></i>
 			Portfolio
 		</Link>
-		<Link to="/market" className="item">
+		<Link to="/market" className="item" onClick={e => this.handleClick(e)}>
 			<i className="line chart icon"></i>
 			Market
 		</Link>
-		<Link to="/news" className="item">
+		<Link to="/news" className="item" onClick={e => this.handleClick(e)}>
 			<i className="newspaper icon"></i>
 			News
 		</Link>
-		<Link to="/leaderboard" className="item">
+		<Link to="/leaderboard" className="item" onClick={e => this.handleClick(e)}>
 			<i className="trophy icon"></i>
 			Leaderboard
 		</Link>
-		<Link to="/companies" className="item">
+		<Link to="/companies" className="item" onClick={e => this.handleClick(e)}>
 			<i className="suitcase icon"></i>
 			Companies
 		</Link>
 		
-		<Link to="/help" className="item">
+		<Link to="/help" className="item" onClick={e => this.handleClick(e)}>
 			<i className="help circle icon"></i>
 			Help
 		</Link>
 		
-		<Link to="/logout" className="item">
+		<Link to="/logout" className="item" onClick={e => this.handleClick(e)}>
 			<i className="window close icon"></i>
 			Logout
 		</Link>
