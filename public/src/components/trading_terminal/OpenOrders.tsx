a34ef139e22b07b77ff7086b0ee6a99a87f514ad
coderick14 <bhattacharyya.rick14@gmail.com>
2018-01-20 06:14:39 +0530

    API integration for Open Orders
    - fetchExistingOpenOrders done
    - TODO : subscription to stream

--- public/src/components/trading_terminal/OpenOrders.tsx
+++ public/src/components/trading_terminal/OpenOrders.tsx
@@ -1,41 +1,99 @@
 import * as React from "react";
 import { Metadata } from "grpc-web-client";
 import { Fragment } from "react";
+import { DalalActionService } from "../../../proto_build/DalalMessage_pb_service";
+import { GetMyOpenOrdersRequest, GetMyOpenOrdersResponse } from "../../../proto_build/actions/GetMyOrders_pb";
+import { Ask as Ask_pb } from "../../../proto_build/models/Ask_pb";
+import { Bid as Bid_pb } from "../../../proto_build/models/Bid_pb";
 
 export interface OpenOrdersProps{
 	sessionMd: Metadata
 }
 
-export class OpenOrders extends React.Component<OpenOrdersProps, {}> {
-	render() {
-		const currentOrders = [
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Buy", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"},
-			{"company":"GOOGL", "type":"Sell", "volume":200, "filled":100, "price":"10"}
-		];
+interface OpenOrdersState {
+	isLoading: boolean,
+	openAsks: Ask_pb[],
+	openBids: Bid_pb[]
+}
 
-		let orderClass;
+export class OpenOrders extends React.Component<OpenOrdersProps, OpenOrdersState> {
+	constructor(props: OpenOrdersProps) {
+		super(props);
+		this.state = {
+			isLoading: false,
+			openAsks: [],
+			openBids: []
+		};
+	}
 
-		let history:any[] = currentOrders.map((order) =>{
-			orderClass = (order.type == "Sell" ? "red" : "green") ;
-			orderClass = orderClass + " volume";
-			return(
-				<tr>
-				<td className={orderClass}><strong>{order.company}</strong></td>
-				<td className={orderClass}><strong>{order.type}</strong></td>
-				<td className={orderClass}><strong>{order.volume}</strong></td>
-				<td className={orderClass}><strong>{order.filled}</strong></td>
-				<td className={orderClass}><strong>{order.price}</strong></td>
-				</tr>
-			)
+	componentDidMount() {
+		this.setState({
+			isLoading: true
 		});
+		this.fetchExistingOpenOrders();
+	}
+
+	fetchExistingOpenOrders = async () => {
+		const getOpenOrdersRequest = new GetMyOpenOrdersRequest();
+
+		try {
+			const resp = await DalalActionService.getMyOpenOrders(getOpenOrdersRequest, this.props.sessionMd);
+			this.setState({
+				isLoading: false,
+				openAsks: resp.getOpenAskOrdersList(),
+				openBids: resp.getOpenBidOrdersList()			
+			});
+			console.log(resp.getStatusCode(), resp.toObject());
+		}
+		catch(e) {
+			// error could be grpc error or Dalal error. Both handled in exception
+			this.setState({
+				isLoading: false
+			});
+			console.log("Error happened! ", e.statusCode, e.statusMessage, e);
+		}
+	};
+
+	render() {
+
+		if (this.state.isLoading) {
+			return (
+				<Fragment>
+					<div className="ui pointing secondary menu">
+						<h3 className="panel-header right item">Open Orders</h3>
+					</div>
+					<div className="ui segment">
+					<div className="ui active dimmer">
+						<div className="ui medium text loader">Fetching your orders...</div>
+					</div>
+					<p></p>
+					</div>
+				</Fragment>
+			);
+		}
+
+		const openAsks = this.state.openAsks;
+		const openBids = this.state.openBids;
+
+		let askElements = openAsks.map((openAsk) =>
+			<tr>
+				<td className="red volume"><strong>{openAsk.getStockId()}</strong></td>
+				<td className="red volume"><strong>Sell</strong></td>
+				<td className="red volume"><strong>{openAsk.getStockQuantity()}</strong></td>
+				<td className="red volume"><strong>{openAsk.getStockQuantityFulfilled()}</strong></td>
+				<td className="red volume"><strong>{openAsk.getPrice()}</strong></td>
+			</tr>
+		);
+
+		let bidElements = openBids.map((openBid) =>
+			<tr>
+				<td className="green volume"><strong>{openBid.getStockId()}</strong></td>
+				<td className="green volume"><strong>Buy</strong></td>
+				<td className="green volume"><strong>{openBid.getStockQuantity()}</strong></td>
+				<td className="green volume"><strong>{openBid.getStockQuantityFulfilled()}</strong></td>
+				<td className="green volume"><strong>{openBid.getPrice()}</strong></td>
+			</tr>
+		);
 		
 		return (
 			<Fragment>
@@ -53,7 +111,8 @@ export class OpenOrders extends React.Component<OpenOrdersProps, {}> {
 						</tr>
 					</thead>
 					<tbody>
-						{history}
+						{askElements}
+						{bidElements}
 					</tbody>
 				</table>
 			</Fragment>
