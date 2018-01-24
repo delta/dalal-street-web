ee2e42b08182cee699535477e4cc7d484becde92
coderick14 <bhattacharyya.rick14@gmail.com>
2018-01-24 22:38:52 +0530

    Add props for Dashboard and Portfolio

--- public/src/components/portfolio/Portfolio.tsx
+++ public/src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import { Metadata } from "grpc-web-client";
+import { Dashboard } from "./Dashboard";
+import { Transactions } from "./Transactions";
+import { Notification } from "../common/Notification";
+import { Notification as Notification_pb } from "../../../proto_build/models/Notification_pb";
+
+// Chart will be exposed globally by the ChartJS script included in index.html
+declare var Chart: any;
+declare var moment: any;
 
 export interface PortfolioProps {
-
+    sessionMd: Metadata,
+    notifications: Notification_pb[],
+    userCash: number,
+    userTotal: number,
+    transactionCount: number,
 }
 
 interface PortfolioState {
@@ -14,6 +27,20 @@ export class Portfolio extends React.Component<PortfolioProps, PortfolioState> {
     }
 
     render() {
-        return <h1>portfolio</h1>
+        return (
+            <div id="" className="main-container ui stackable grid pusher">
+				<div className="row" id="top_bar">
+					<div id="notif-component">
+						<Notification notifications={this.props.notifications} icon={"open envelope icon"} />
+					</div>
+				</div>
+                <div id="dashboard-container" className="row">
+                    <Dashboard userCash={this.props.userCash} userTotal={this.props.userTotal} />
+                </div>
+                <div id="transactions-container" className="row">
+                    <Transactions sessionMd={this.props.sessionMd} count={this.props.transactionCount}/>
+                </div>
+            </div>
+        );
     }
 }
