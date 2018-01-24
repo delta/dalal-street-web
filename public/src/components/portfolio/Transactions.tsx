9216f5da07d56aae789a839b69528292c996a54d
paiakshay998 <paiakshay998@gmail.com>
2018-01-24 22:40:05 +0530

    finished somewhat transaction

--- public/src/components/portfolio/Transactions.tsx
+++ public/src/components/portfolio/Transactions.tsx
@@ -1,19 +1,40 @@
 import * as React from "react";
+import { Fragment } from "react";
+import { Metadata } from "grpc-web-client";
+
 
 export interface TransactionsProps {
-
+    sessionMd: Metadata,
+    count: number,
 }
 
-interface TransactionsState {
-
-}
-
-export class Transactions extends React.Component<TransactionsProps, TransactionsState> {
+export class Transactions extends React.Component<TransactionsProps, {}> {
     constructor(props: TransactionsProps) {
         super(props);
     }
 
     render() {
-        return <h1>transactions</h1>
+        return (
+            <Fragment>
+                <div className="ui pointing secondary menu">
+                    <h3 className="panel-header right item">Open Orders</h3>
+                </div>
+                <table className="ui inverted table unstackable">
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Type</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                            <th>Date Bought</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </Fragment>
+        )
     }
 }
