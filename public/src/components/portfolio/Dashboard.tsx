5ba15ef18153075fe2ecff46028d50f344aa9ca3
coderick14 <bhattacharyya.rick14@gmail.com>
2018-01-25 19:35:08 +0530

    Add portfolio component
    - Transactions stream not working
    - Need to make design changes

--- public/src/components/portfolio/Dashboard.tsx
+++ public/src/components/portfolio/Dashboard.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
+import { Fragment } from "react";
+
+import { StockBriefInfo } from "../trading_terminal/TradingTerminal";
+
+// Chart will be exposed globally by the ChartJS script included in index.html
+declare var Chart: any;
+declare var moment: any;
 
 export interface DashboardProps {
     userCash: number,
     userTotal: number,
+    stockBriefInfoMap: { [index:number]: StockBriefInfo },
+    chartData: { [index:number]: number }
 }
 
 interface DashboardState {
@@ -10,11 +19,83 @@ interface DashboardState {
 }
 
 export class Dashboard extends React.Component<DashboardProps, DashboardState> {
+    chartElem: any;
     constructor(props: DashboardProps) {
         super(props);
     }
 
+    componentDidMount() {
+		const cvs = document.getElementById("doughnut-chart") as HTMLCanvasElement;
+		const ctx = cvs!.getContext('2d')!;
+
+        const data = Object.values(this.props.chartData).filter(qty => qty > 0);
+        let labels: string[] = [];
+
+        for (const stockId in this.props.stockBriefInfoMap) {
+            labels.push(this.props.stockBriefInfoMap[stockId].fullName);
+        }
+
+
+		this.chartElem = new Chart(ctx, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+                    data: data,
+                    backgroundColor: ["#03A4E2"],
+                    borderWidth: 0
+                }],
+                labels: labels
+            },
+            options: {
+                legend: {
+					display: true,
+				},
+            }
+        });
+    }
+
+    componentWillReceiveProps(nextProps: DashboardProps) {
+        if (nextProps.chartData) {
+            let labels: string[] = [];
+
+            for (const stockId in this.props.stockBriefInfoMap) {
+                labels.push(this.props.stockBriefInfoMap[stockId].fullName);
+            }
+            this.chartElem.data.datasets = [{
+                data: Object.values(nextProps.chartData).filter(qty => qty > 0),
+                backgroundColor: ["#03A4E2"],
+                borderWidth: 0  
+            }];
+            this.chartElem.data.labels = labels;
+			this.chartElem.update();
+        }
+    }
+
     render() {
-        return <h1>Dashboard</h1>
+        return (
+            <Fragment>
+                <div className="ui seven wide column" >
+                    <canvas id="doughnut-chart"></canvas>
+                </div>
+                <div className="ui three wide column">
+                    <h1 className="ui center aligned green header inverted">
+                        ₹ {this.props.userCash}
+                        <div className="sub header">Cash in Hand</div>
+                    </h1>
+                </div>
+                <div className="ui three wide column">
+                    <h1 className="ui center aligned green header inverted">
+                        ₹ {this.props.userTotal - this.props.userCash}
+                        <div className="sub header">Stock Worth</div>
+                    </h1>
+                </div>
+                <div className="ui three wide column">
+                    <h1 className="ui center aligned green header inverted">
+                        ₹ {this.props.userTotal}
+                        <div className="sub header">Net Worth</div>
+                    </h1>
+                </div>
+            </Fragment>
+        );
     }
 }
--- public/src/components/portfolio/Transactions.tsx
+++ public/src/components/portfolio/Transactions.tsx
@@ -2,36 +2,146 @@ import * as React from "react";
 import { Fragment } from "react";
 import { Metadata } from "grpc-web-client";
 
+import { subscribe, unsubscribe } from "../../streamsutil";
+import { DataStreamType, SubscriptionId } from "../../../proto_build/datastreams/Subscribe_pb";
+import { DalalActionService, DalalStreamService } from "../../../proto_build/DalalMessage_pb_service";
+
+import { GetTransactionsRequest } from "../../../proto_build/actions/GetTransactions_pb";
+import { TransactionUpdate } from "../../../proto_build/datastreams/Transactions_pb";
+import { Transaction as Transaction_pb, TransactionType } from "../../../proto_build/models/Transaction_pb";
+
+import { StockBriefInfo } from "../trading_terminal/TradingTerminal";
+
+const FROM_EXCHANGE_TRANSACTION = TransactionType.FROM_EXCHANGE_TRANSACTION;
+const ORDER_FILL_TRANSACTION = TransactionType.ORDER_FILL_TRANSACTION;
+const MORTGAGE_TRANSACTION = TransactionType.MORTGAGE_TRANSACTION;
+const DIVIDEND_TRANSACTION = TransactionType.DIVIDEND_TRANSACTION;
+
+const transactionTypeToStr = (trType: TransactionType): string => {
+    switch(trType) {
+		case FROM_EXCHANGE_TRANSACTION : return "Exchange";
+		case ORDER_FILL_TRANSACTION : return "OrderFill";
+		case MORTGAGE_TRANSACTION : return "Mortgage";
+		case DIVIDEND_TRANSACTION : return "Dividend";
+    }
+    return "";
+}
 
 export interface TransactionsProps {
     sessionMd: Metadata,
-    count: number,
+    transactionCount: number,
+    stockBriefInfoMap: { [index:number]: StockBriefInfo },
+    transactionUpdatesCallback: (stockId: number, stockQuantity: number, total: number) => void,
 }
 
-export class Transactions extends React.Component<TransactionsProps, {}> {
+interface TransactionsState {
+    subscriptionId: SubscriptionId,
+    transactions: Transaction_pb[],
+}
+
+export class Transactions extends React.Component<TransactionsProps, TransactionsState> {
     constructor(props: TransactionsProps) {
         super(props);
+
+        this.state = {
+            subscriptionId: new SubscriptionId,
+            transactions: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getOldTransactions();
+    }
+
+    componentWillUnmount() {
+        unsubscribe(this.props.sessionMd, this.state.subscriptionId);
+    }
+
+    getOldTransactions = async () => {
+        const transactionsRequest = new GetTransactionsRequest();
+
+        // lastTransactionId = 0 fetches the latest transactions
+        transactionsRequest.setLastTransactionId(0);
+        transactionsRequest.setCount(this.props.transactionCount); 
+
+        try {
+            const resp = await DalalActionService.getTransactions(transactionsRequest, this.props.sessionMd);
+            console.log(resp.getStatusCode(), resp.toObject());
+            this.setState({
+                transactions: resp.getTransactionsList()
+            });
+
+            this.handleTransactionsStream();
+        } catch(e) {
+            // error could be grpc error or Dalal error. Both handled in exception
+			console.log("Error happened! ", e.statusCode, e.statusMessage, e);
+        }
+    }
+
+    handleTransactionsStream = async () => {
+        const props = this.props;
+        const subscriptionId = await subscribe(props.sessionMd, DataStreamType.TRANSACTIONS);
+
+		this.setState({
+			subscriptionId: subscriptionId,
+		});
+
+		const stream = DalalStreamService.getTransactionUpdates(subscriptionId, props.sessionMd);
+        for await (const update of stream) {
+            alert("bhai bhai naya transaction");
+            const newTransaction = update.getTransaction()!;
+            this.props.transactionUpdatesCallback(
+                newTransaction.getStockId(),
+                newTransaction.getStockQuantity(),
+                newTransaction.getTotal());
+
+            let updatedTransactions = this.state.transactions.slice();
+            updatedTransactions.unshift(newTransaction);
+
+            this.setState({
+                transactions: updatedTransactions,
+            });
+        }
     }
 
     render() {
+        const currentTransactions = this.state.transactions;
+        let transactionsContent = currentTransactions.map((transaction) => (
+            <tr key={transaction.getId()}>
+                <td>
+                    <strong>
+                        {this.props.stockBriefInfoMap[transaction.getStockId()].fullName}
+                    </strong>
+                </td>
+                <td><strong>{transactionTypeToStr(transaction.getType())}</strong></td>
+                <td className={transaction.getStockQuantity() >= 0 ? "green" : "red"}>
+                    <strong>{transaction.getStockQuantity()}</strong>
+                </td>
+                <td><strong>{transaction.getPrice()}</strong></td>
+                <td className={transaction.getTotal() >= 0 ? "green" : "red"}>
+                    <strong>{transaction.getTotal()}</strong>
+                </td>
+                <td><strong>{transaction.getCreatedAt()}</strong></td>
+            </tr>
+        ));
         return (
             <Fragment>
-                <div className="ui pointing secondary menu">
-                    <h3 className="panel-header right item">Open Orders</h3>
-                </div>
                 <table className="ui inverted table unstackable">
                     <thead>
+                        <tr><th colSpan={6} className="ui white header">
+                            Your transactions
+                        </th></tr>
                         <tr>
                             <th>Company</th>
                             <th>Type</th>
                             <th>Quantity</th>
                             <th>Price</th>
                             <th>Total</th>
-                            <th>Date Bought</th>
+                            <th>Time</th>
                         </tr>
                     </thead>
                     <tbody>
-
+                        {transactionsContent}
                     </tbody>
                 </table>
             </Fragment>
