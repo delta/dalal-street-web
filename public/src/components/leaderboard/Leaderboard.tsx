b5d3ebce293677f022cd9edc729808a7a50d7567
coderick14 <bhattacharyya.rick14@gmail.com>
2018-02-19 12:26:34 +0530

    Actually fix spacing in leaderboard

--- public/src/components/leaderboard/Leaderboard.tsx
+++ public/src/components/leaderboard/Leaderboard.tsx
@@ -108,14 +108,14 @@ export class Leaderboard extends React.Component<LeaderboardProps, LeaderboardSt
                     
                     <div id="my-rank" className="row">
                         <div className="one wide column"></div>
-                        <div className="nine wide column">
+                        <div className="eight wide column">
                             <div className="ui header inverted">
                                 <i className="star icon"></i>
                                 Your rank : {state.userRank}
                             </div>
                         </div>
-                        <div className="five wide column">
-                            <div className="ui small header inverted">
+                        <div className="six wide column">
+                            <div className="ui small right aligned header inverted">
                                 Leaderboard will be updated every 2 minutes
                             </div>
                         </div>
