1cd241a187559cf550d29d1ee7fbaed338dbc4ee
Adwaith <adwaith3208@gmail.com>
2019-01-28 18:42:01 +0530

    [Bug Fix]: Fix X-axis scale of graphs

--- public/src/components/trading_terminal/charts/lineChart.tsx
+++ public/src/components/trading_terminal/charts/lineChart.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { Fragment } from "react";
 
-import { ohlcPointType, intervalType } from "./types";
+import { ohlcPointType, intervalType, ohlcvPointType } from "./types";
 import { getUnit } from "./utils";
 
 export interface LineChartProps {
     stockId: number
     tabName: string
-	data: ohlcPointType[]
+	data: ohlcvPointType[]
 	interval: intervalType
 }
 
 export interface LineChartState {
     stockId: number
-    data: ohlcPointType[]
+    data: ohlcvPointType[]
 }
 
 // Chart will be exposed globally by the ChartJS script included in index.html
@@ -29,22 +29,32 @@ export class LineChart extends React.Component<LineChartProps, LineChartState> {
             data: props.data
 		};
 	}
-
+  
     // convert ohlcPointType to { x: time, y: closePrice }
-    ohlcToOnlyC(p: ohlcPointType) {
+    ohlcvToOnlyC(p: ohlcvPointType) {
         const d = new Date();
         d.setTime(p.t);
-        return { x: d, y: p.c };
-    }
+		return p.c;
+	}
+	
+	ohlcvToDate(p: ohlcvPointType){
+        const d = new Date();
+        d.setTime(p.t);
+		return d.toLocaleTimeString('en-US', {hour12: false}).slice(0,5);
+	}
 
 	componentWillReceiveProps(nextProps: LineChartProps) {
 		// update the data and the chart, don't render the thing again pls
 		if (nextProps.data) {
-			this.chartElem.data.datasets = [{
-				data: nextProps.data.map(this.ohlcToOnlyC),
-				fractionalDigitsCount: 2,
-			}];
-			this.chartElem.options.scales.xAxes[0].time.unit = getUnit(this.props.interval);
+			this.chartElem.data = {
+				labels: nextProps.data.slice(-20).map(this.ohlcvToDate),
+				datasets: [{
+				data: nextProps.data.slice(-20).map(this.ohlcvToOnlyC),
+				// fractionalDigitsCount: 2,
+				borderColor: "#ffffff",
+				pointBackgroundColor: "#ffffff",
+			}]};
+			// this.chartElem.options.scales.xAxes[0].time.unit = getUnit(this.props.interval);
 			this.chartElem.update();
 		}
     }
@@ -53,31 +63,20 @@ export class LineChart extends React.Component<LineChartProps, LineChartState> {
 		const cvs = document.getElementById("line-chart") as HTMLCanvasElement;
 		const ctx = cvs!.getContext('2d')!;
 
-        const data = this.state.data.map(this.ohlcToOnlyC);
+        const data = this.state.data;
 
 		this.chartElem = new Chart(ctx, {
 			type: 'line',
 			data: {
+				labels: data.slice(-10).map(this.ohlcvToDate) ,
 				datasets: [{
-					data: data,
-					fractionalDigitsCount: 2,
+					data: data.slice(-20).map(this.ohlcvToOnlyC),
+					// fractionalDigitsCount: 2,
+					borderColor: "#ffffff",
+					pointBackgroundColor: "#ffffff",
 				}]
 			},
 			options: {
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							unit: getUnit(this.props.interval),
-							tooltipFormat: "ddd MMM Do h:mm a",
-						},
-						ticks: {
-							autoSkip: true,
-							maxTicksLimit: 20,
-							maxRotation: 0,
-						}
-					}]
-				},
 				legend: {
 					display: false,
 				},
--- public/src/components/trading_terminal/charts/volume.tsx
+++ public/src/components/trading_terminal/charts/volume.tsx
@@ -32,19 +32,26 @@ export class VolumeChart extends React.Component<VolumeChartProps, VolumeChartSt
 
     // convert ohlcPointType to { x: time, y: closePrice }
     ohlcToOnlyV(p: ohlcvPointType) {
+        return p.v;
+	}
+	
+	ohlcvToDate(p: ohlcvPointType){
         const d = new Date();
         d.setTime(p.t);
-        return { x: d, y: p.v };
-    }
+		return d.toLocaleTimeString('en-US', {hour12: false}).slice(0,5);
+	}
 
 	componentWillReceiveProps(nextProps: VolumeChartProps) {
 		// update the data and the chart, don't render the thing again pls
 		if (nextProps.data) {
-			this.chartElem.data.datasets = [{
-				data: nextProps.data.map(this.ohlcToOnlyV),
+			this.chartElem.data = {
+				labels: nextProps.data.slice(-20).map(this.ohlcvToDate),
+				datasets: [{
+				backgroundColor: "#c45850",
+				data: nextProps.data.map(this.ohlcToOnlyV).slice(-20),
 				fractionalDigitsCount: 2,
-			}];
-			this.chartElem.options.scales.xAxes[0].time.unit = getUnit(this.props.interval);
+			}]
+		};
 			this.chartElem.update();
 		}
     }
@@ -53,31 +60,19 @@ export class VolumeChart extends React.Component<VolumeChartProps, VolumeChartSt
 		const cvs = document.getElementById("volume-chart") as HTMLCanvasElement;
 		const ctx = cvs!.getContext('2d')!;
 
-        const data = this.state.data.map(this.ohlcToOnlyV);
+        const data = this.state.data;
 
 		this.chartElem = new Chart(ctx, {
-			type: 'line',
+			type: 'bar',
 			data: {
+				labels: data.slice(-20).map(this.ohlcvToDate),
 				datasets: [{
-					data: data,
+					backgroundColor: "#c45850",
+					data: data.slice(-20).map(this.ohlcToOnlyV),
 					fractionalDigitsCount: 2,
 				}]
 			},
 			options: {
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							unit: getUnit(this.props.interval),
-							tooltipFormat: "ddd MMM Do h:mm a",
-						},
-						ticks: {
-							autoSkip: true,
-							maxTicksLimit: 20,
-							maxRotation: 0,
-						}
-					}]
-				},
 				legend: {
 					display: false,
 				},
