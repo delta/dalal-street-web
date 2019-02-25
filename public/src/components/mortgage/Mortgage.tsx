4cdca2b463a16be0bb155e8f75b3e9a1aec15aa5
ishanisri <ishasri.1212@gmail.com>
2019-02-25 08:50:09 +0530

    [general]: Add popup to buttons

--- public/src/components/mortgage/Mortgage.tsx
+++ public/src/components/mortgage/Mortgage.tsx
@@ -204,8 +204,6 @@ export class Mortgage extends React.Component<MortgageProps, MortgageState> {
     }
 
     render() {
-        $("#mortgage-button").disabled = this.props.isMarketOpen;
-        $("#retrieve-button").disabled = this.props.isMarketOpen;
         const stockBriefInfoMap = this.props.stockBriefInfoMap;
         const stockPricesMap = this.props.stockPricesMap;
         const stocksOwnedMap = this.props.stocksOwnedMap;
@@ -223,7 +221,7 @@ export class Mortgage extends React.Component<MortgageProps, MortgageState> {
                     <td><strong>{this.props.depositRate + "%"}</strong></td>
                     <td className="green"><strong>{(stockPricesMap[stockId] * this.props.depositRate) / 100}</strong></td>
                     <td><strong><input id={"mortgageinput-" + stockId} placeholder="0" className="mortgage-input" /></strong></td>
-                    <td><strong><button id="mortgage-button" className="ui inverted green button" onClick={() => { this.mortgageStocks(Number(stockId)) }}>Mortgage</button></strong></td>
+                    <td><div data-tooltip="This is used to mortgage stocks" data-position="top center"><strong><button disabled={this.props.isMarketOpen ? false : true} className="ui inverted green button" onClick={() => { this.mortgageStocks(Number(stockId)) }}>Mortgage</button></strong></div></td>
                 </tr>
             );
         }
@@ -241,7 +239,7 @@ export class Mortgage extends React.Component<MortgageProps, MortgageState> {
                         <td><strong>{this.props.retrieveRate + "%"}</strong></td>
                         <td className="green"><strong>{(mortgagePrice * this.props.retrieveRate) / 100}</strong></td>
                         <td><strong><input id={"retrieveinput-" + uniqueKey} placeholder="0" className="mortgage-input" /></strong></td>
-                        <td><strong><button id="retireve-button" className="ui inverted green button" onClick={() => { this.retrieveStocks(Number(stockId), mortgagePrice) }}>Retrieve</button></strong></td>
+                        <td><div  data-tooltip="This is used to retrieve stocks" data-position="top center"><strong><button disabled={this.props.isMarketOpen ? false : true} className="ui inverted green button" onClick={() => { this.retrieveStocks(Number(stockId), mortgagePrice) }}>Retrieve</button></strong></div></td>
                     </tr>
                 );
             });
