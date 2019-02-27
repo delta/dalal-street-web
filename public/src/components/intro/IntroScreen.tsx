e608ce74faa8cf3c5e9ab2fb78279b160502fec8
ishanisri <ishasri.1212@gmail.com>
2019-02-27 20:34:43 +0530

    [intropage]: add DO logo

--- public/src/components/intro/IntroScreen.tsx
+++ public/src/components/intro/IntroScreen.tsx
@@ -13,19 +13,25 @@ export class IntroScreen extends React.Component<{}, {}> {
                         <span id="pragyan-logo">
                             <img className="image" src="./public/src/images/pragyanlogo.png" />
                         </span>
+                        <div id="cloud-logo">
+                        <p> Cloud partner</p>
+                          <img  className="image" src="./public/src/images/DO_Logo_horizontal_white.svg" />
+                        </div>
+                        <div id="text">
                         <h1>Dalal Street</h1>
                         <p>The stock market gaming engine</p>
+                      </div>
                     </div>
-        
+
                     <div id="main">
-        
+
                         <header className="major container 75%">
                             <h3>If stock market experts were so expert,<br/>
                                 they would be buying stock, not selling advice.<br/>
                                 - Norman Ralph Augustine
                             </h3>
                         </header>
-        
+
                         <div className="box alt container">
                             <section className="feature left">
                                 <a className="image"><img src="./public/src/images/pic01.png" alt="" /></a>
@@ -34,15 +40,15 @@ export class IntroScreen extends React.Component<{}, {}> {
                                     <p className="p-text">
                                     Dalal Street is an online virtual stock exchange.
                                     The game will run for 7 days and your goal is to
-                                    maximise your profit. Once you login, please read our 
+                                    maximise your profit. Once you login, please read our
                                     <strong> Getting Started </strong> section on the Help Page
                                     before playing the game.
                                     </p>
                                 </div>
                             </section>
                         </div>
-        
-        
+
+
                         <footer className="major container 75%">
                             <h3>New to stock markets?</h3>
                             <p className="p-text">Don't worry. We've got a list of resouces to get you
@@ -53,25 +59,25 @@ export class IntroScreen extends React.Component<{}, {}> {
                                 <li><a href="/register" className="intro-button">Get Started</a></li>
                             </ul>
                         </footer>
-        
+
                     </div>
-        
+
                     <div id="footer">
                         <div className="container 75%">
-        
+
                             <header className="major last">
                                 <h2>Questions or comments?</h2>
                             </header>
-        
+
                             <p>Reach out to us on <a href="https://www.allanswered.com/community/s/dalal-street/">the Dalal Forum</a></p>
-        
-                            <ul className="copyright">
-                                <li>&copy; Made with <span className="red">&hearts;</span> by <a href="https://delta.nitt.edu">Delta Force</a></li>
-                            </ul>
-        
+
+
+                                &copy; Made with <span className="red">&hearts;</span> by <a href="https://delta.nitt.edu">Delta Force</a>
+
+
                         </div>
                     </div>
-        
+
             </Fragment>
         );
     }
