0acc9bfd67fcf8bc21e26b6d16dee8e1459983c9
Meivenkatkumar <ln.meivenkatkumar21@gmail.com>
2020-02-01 15:04:57 +0530

    [MarketState]: close/open market in admin panel

--- public/src/components/Main.tsx
+++ public/src/components/Main.tsx
@@ -621,6 +621,7 @@ export class Main extends React.Component<MainProps, MainState> {
                     sessionMd={this.props.sessionMd}
                     stockBriefInfoMap={this.state.stockBriefInfoMap}
                     stockPricesMap={this.getStockPrices(this.state.stockDetailsMap)}
+                    isMarketOpen={this.state.isMarketOpen}
                 />
             default:
                 return <NotFound />;
