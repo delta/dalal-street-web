b248c86807b182d7cbeada20103c0776a11fc3a8
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2020-02-28 18:13:02 +0530

    [OTP]: Add Resend OTP Timer

--- public/src/components/mobile_verification/Timer.tsx
+++ public/src/components/mobile_verification/Timer.tsx
@@ -4,42 +4,71 @@ import { Metadata } from "grpc-web-client";
 export interface TimerState {
   minutes : number
   seconds: number
+  resetStatus: boolean
 }
 
-export class Timer extends React.Component<{}, TimerState> {
-  constructor(props: TimerState) {
+export interface TimerProps{
+    handleResendOtpCallback: () => void
+    resetTimer: boolean
+}
+
+export class Timer extends React.Component<TimerProps, TimerState> {
+  constructor(props: TimerProps) {
     super(props);
 
         this.state= {
         minutes: 1,
         seconds: 0,
+        resetStatus: true
     }
   }
 
-    componentDidMount() {
-        let intervalId = setInterval(() => {
-            const { seconds, minutes } = this.state
+  timer = () => {
+    let intervalId = setInterval(() => {
+        const { seconds, minutes } = this.state
 
-            if (seconds > 0) {
-                this.setState(({ seconds:number }) => ({
-                    seconds: seconds - 1
+        if (seconds > 0) {
+            this.setState(({ seconds:number }) => ({
+                seconds: seconds - 1
+            }))
+        }
+        if (seconds === 0) {
+            if (minutes === 0) {
+                this.props.handleResendOtpCallback();
+                this.setState({
+                    minutes: minutes,
+                    seconds: seconds
+                })
+                clearInterval(intervalId);
+            } else {
+                this.setState(({ minutes: minutes }) => ({
+                    minutes: minutes - 1,
+                    seconds: 59
                 }))
             }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    //clearInterval(intervalId)
-                } else {
-                    this.setState(({ minutes: minutes }) => ({
-                        minutes: minutes - 1,
-                        seconds: 59
-                    }))
-                }
+        }
+    }, 1000)
+  }
+
+  componentWillReceiveProps(){
+
+        if(this.props.resetTimer && this.state.resetStatus){
+            this.setState( {
+                minutes: 1,
+                seconds: 0,
+            })
+            this.timer();
+        }
+        this.setState((prevState) => {
+            return{
+                resetStatus: !prevState.resetStatus
             }
-        }, 1000)
+        })
+    
     }
 
     componentWillUnmount() {
-        //clearInterval(this.myInterval)
+        // clearInterval(this.timer)
     }
 
     render() {
@@ -47,7 +76,7 @@ export class Timer extends React.Component<{}, TimerState> {
         return (
             <div>
                 { minutes === 0 && seconds === 0
-                    ? <strong>Click on resend OTP</strong>
+                    ? <strong>Resend OTP</strong>
                     : <strong>Resend OTP in: 0{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</strong>
                 }
             </div>
