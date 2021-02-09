66bff5ab11de46b257b56e7ca6cee9dc84c4bf02
kelpikz <ajithaprasad96@gmail.com>
2021-02-09 20:56:30 +0530

    Feat : dd title and message to notification body

--- public/assets/serviceWorker.js
+++ public/assets/serviceWorker.js
@@ -1,10 +1,12 @@
 console.log("Hello world from service worker");
 
 self.addEventListener("push", (e) => {
+  console.log(e);
   const data = e.data.json();
+  console.log(data);
   console.log("Push Received...", data);
-  self.registration.showNotification(data.title, {
-    body: data.body,
+  self.registration.showNotification(data.Title, {
+    body: data.Message,
     icon: "",
   });
 });
