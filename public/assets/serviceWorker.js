42636f8a45251db29cffecc17b846edebd2cbb98
0149Sailesh <63252491+0149Sailesh@users.noreply.github.com>
2021-03-10 17:42:07 +0530

    Feat: Added offline page (#187)
    * Feat: Added offline page
    
    * Added new line

--- public/assets/serviceWorker.js
+++ public/assets/serviceWorker.js
@@ -1,5 +1,4 @@
 console.log("Hello world from service worker");
-
 self.addEventListener("push", (e) => {
   console.log(e);
   const data = e.data.json();
@@ -9,4 +8,40 @@ self.addEventListener("push", (e) => {
     body: data.Message,
     icon: "",
   });
+});
+var currentCache = {
+  offline: 'offline-cache_dalal'
+};
+const offlineUrl = 'offline.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+        "./sed.png",
+          offlineUrl
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
+    );
+  }
+  else{
+    // Respond with everything else if we can
+    event.respondWith(caches.match(event.request)
+                    .then(function (response) {
+                    return response || fetch(event.request);
+                }).catch(error =>{
+                  return caches.match("./sed.png");
+                })
+        );
+  }
 });
