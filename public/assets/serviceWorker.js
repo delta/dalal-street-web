68e932040588ffbfd9d576f406cf2f0ca8370f9a
kelpikz <ajithaprasad96@gmail.com>
2021-03-19 13:54:00 +0530

    Fix: Temp update of logo url

--- public/assets/serviceWorker.js
+++ public/assets/serviceWorker.js
@@ -8,6 +8,8 @@ self.addEventListener("push", (e) => {
     body: data.Message,
     requireInteraction: true,
     icon: data.LogoUrl,
+    badge: data.LogoUrl,
+    image: data.LogoUrl,
   });
 });
 
@@ -42,38 +44,37 @@ self.addEventListener("notificationclick", function (e) {
 });
 
 var currentCache = {
-  offline: 'offline-cache_dalal'
+  offline: "offline-cache_dalal",
 };
-const offlineUrl = 'offline.html';
+const offlineUrl = "offline.html";
 
-this.addEventListener('install', event => {
+this.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-        "./sed.png",
-          offlineUrl
-      ]);
+    caches.open(currentCache.offline).then(function (cache) {
+      return cache.addAll(["./sed.png", offlineUrl]);
     })
   );
 });
 
-this.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
+this.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request.url).catch((error) => {
+        // Return the offline page
+        return caches.match(offlineUrl);
+      })
     );
-  }
-  else{
+  } else {
     // Respond with everything else if we can
-    event.respondWith(caches.match(event.request)
-                    .then(function (response) {
-                    return response || fetch(event.request);
-                }).catch(error =>{
-                  return caches.match("./sed.png");
-                })
-        );
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(function (response) {
+          return response || fetch(event.request);
+        })
+        .catch((error) => {
+          return caches.match("./sed.png");
+        })
+    );
   }
 });
