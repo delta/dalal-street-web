733cce3dc267d8dc21d899bcae92bfe754ea14ab
kelpikz <ajithaprasad96@gmail.com>
2021-03-28 04:30:32 +0530

    feat: add resend email feature

--- public/src/components/login/LoginForm.tsx
+++ public/src/components/login/LoginForm.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { LoginRequest, LoginResponse, } from "../../../proto_build/actions/Login_pb";
+import { ResendVerificationEmailRequest, ResendVerificationEmailResponse } from "../../../proto_build/actions/ResendVerificationEmail_pb"
 import { DalalActionService } from "../../../proto_build/DalalMessage_pb_service";
 
 export interface LoginFormProps {
@@ -11,6 +12,7 @@ export interface LoginFormState {
     password: string,
     disabled: boolean,
     error: string | null,
+    success: string | null,
 }
 
 export class LoginForm extends React.Component<LoginFormProps, LoginFormState>{
@@ -21,6 +23,7 @@ export class LoginForm extends React.Component<LoginFormProps, LoginFormState>{
             email: "",
             disabled: false,
             error: null,
+            success: null
         };
     }
 
@@ -60,10 +63,27 @@ export class LoginForm extends React.Component<LoginFormProps, LoginFormState>{
         });
     }
 
+    handleResendVerificationEmail = async () => {
+
+        const resendVerificationEmailRequest = new ResendVerificationEmailRequest();
+        resendVerificationEmailRequest.setEmail(this.state.email);
+        try {
+            const resp = await DalalActionService.resendVerificationEmai(resendVerificationEmailRequest);
+            this.setState({
+                error: null,
+                success: "Successfully resent verification Email"
+            });
+        } catch(err) {
+            this.setState({
+                error: err.isGrpcError ? "Unable to reach server. Please check your internet connection." : err.statusMessage,
+            });
+        }
+    }
+
     render() {
         return (
             <div>
-                <form className="ui large form">
+                <form className="ui large form" action="javascript:void(0)">
                     <div className="ui attatched stacked secondary segment">
                         <div className="field">
                             <div className="ui left icon input">
@@ -78,15 +98,17 @@ export class LoginForm extends React.Component<LoginFormProps, LoginFormState>{
                             </div>
                         </div>
                         {this.state.disabled ?
-                            <div className="ui fluid large teal submit disabled button">Login</div> :
-                            <div className="ui fluid large teal submit button" onClick={this.handleLogin}>Login</div>
+                            <button className="ui fluid large teal submit disabled button">Login</button> :
+                            <button className="ui fluid large teal submit button" onClick={this.handleLogin}>Login</button>
                         }
                     </div>
                 </form>
-                {this.state.error != null && <div className="ui negative bottom attached message">
+                { this.state.error != null &&<div className="ui negative bottom attached message">
                     <i className="icon error"></i>
-                    {this.state.error}
-                </div>}
+                    { this.state.error === "User has not verified account" ? 
+                    <span> Email hasn't been verified yet. <a className="register-hover" onClick={this.handleResendVerificationEmail} >Resend verification email.</a> </span> 
+                    : this.state.error }
+                </div> }
             </div>
         );
     }
